7a9c8a46b94f9aaa95bebdf87c6492b4375ae92e
sam1360 <sam1360@g.rit.edu>
2017-03-28 19:50:35 -0400

    Minor code style changes

--- routes/v01/citation.js
+++ routes/v01/citation.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 var citations = require('../../lib/citationGeneration');
 
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
   res.send('Citation web server api');
 });
 
--- routes/v01/index.js
+++ routes/v01/index.js
@@ -7,11 +7,11 @@ router.use('/citation', require('./citation'));
 // used so Angular may be able to log out by 
 // simply destroying the token
 
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
   res.send('Citation System API');
 });
 
-router.get('/about', function(req, res) {
+router.get('/about', (req, res) => {
   res.send('api.SoftwareCitationTools.com');
 });
 
