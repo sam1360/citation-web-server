0da5c2221973b896719802ce3ed6f66f6318ae83
sam1360 <sam1360@g.rit.edu>
2017-03-28 19:54:37 -0400

    more minor changes

--- routes/v01/citation.js
+++ routes/v01/citation.js
@@ -1,8 +1,7 @@
-"use strict";
+const express = require('express');
+const citations = require('../../lib/citationGeneration');
 
-var express = require('express');
-var router = express.Router();
-var citations = require('../../lib/citationGeneration');
+const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('Citation web server api');
